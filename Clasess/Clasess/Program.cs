f2529bb33b6a99990e849207c0d5042595db5f95
Maidy Conde <maidyladino04@gmail.com>
2024-08-05 12:43:23 -0500

    c#

---
+++ Clasess/Clasess/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Circulo
+{
+    public double Radio { get; set; }
+
+    public double CalcularArea()
+    {
+        return Math.PI * Radio * Radio;
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Circulo miCirculo = new Circulo();
+
+        miCirculo.Radio = 5;
+
+        double area = miCirculo.CalcularArea();
+
+        Console.WriteLine("El area del circulo con radio " + miCirculo.Radio + " es: " + area);
+    }
+}
+
---
+++ Clasess/claseseinterfaces/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseseinterfaces
+{
+    public class Animal : IAnimal
+    {
+        // Implementación del método HacerSonido
+        public void HacerSonido()
+        {
+            Console.WriteLine("El animal hace un sonido.");
+        }
+
+        // Implementación del método Mover
+        public void Mover()
+        {
+            Console.WriteLine("El animal se está moviendo.");
+        }
+    }
+    public class AnimalProgram
+    {
+        public static void Main(string[] args)
+        {
+            // Crear una instancia de Animal
+            IAnimal miAnimal = new Animal();
+
+            // Llamar a los métodos definidos en la interfaz
+            miAnimal.HacerSonido();
+            miAnimal.Mover();
+        }
+    }
+
+}
